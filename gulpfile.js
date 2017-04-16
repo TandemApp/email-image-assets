b767714cc9545727517e2e7621729775eaddcfef
Neil Soult <neilsoult@users.noreply.github.com>
2017-04-16 14:36:19 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,11 @@ const open = require('gulp-open');
 const plumber = require('gulp-plumber');
 const rs = require('run-sequence');
 
-let config = fsx.readJsonSync('config.json');
+const config = fsx.readJsonSync('config.json');
 const s3 = require('gulp-s3-upload')(config);
 
 let paths = {
-    s3: 'https://s3-us-west-1.amazonaws.com/tandem-email/',
+    s3: config.S3URLPath,
     dest: {
         img: (isGlob) =>  {
             return './build/' + (isGlob ? '**/*' : '');
@@ -28,6 +28,7 @@ let paths = {
 gulp.task('clean', () => {
 
     return fsx.removeSync(paths.dest.img());
+
 });
 
 gulp.task('imagemin', ['clean'], () => {
@@ -36,6 +37,7 @@ gulp.task('imagemin', ['clean'], () => {
     .pipe(plumber())
     .pipe(imagemin())
     .pipe(gulp.dest(paths.dest.img()));
+
 });
 
 gulp.task('upload', () => {
@@ -43,12 +45,13 @@ gulp.task('upload', () => {
     return gulp.src(paths.dest.img(true))
     .pipe(plumber())
     .pipe(s3({
-        Bucket: 'tandem-email',
+        Bucket: config.S3BucketName,
         ACL: 'public-read'
     }, {
         // S3 Constructor options
         maxRetries: 5
     }));
+
 });
 
 gulp.task('open', () => {
@@ -61,6 +64,7 @@ gulp.task('open', () => {
             let path = paths.s3 + file.replace(paths.dest.img(), '');
             html += `<dt>${path}</dt>`;
             html += `<dd><img src="${path}" /></dd>`;
+
         });
         html += '</dl>';
         console.log(html);
@@ -68,10 +72,13 @@ gulp.task('open', () => {
         gulp.src('./index.html')
         .pipe(plumber())
         .pipe(open());
+
     });
+
 });
 
 gulp.task('default', (cb) => {
 
     rs('imagemin', 'upload', 'open', cb);
+
 });
